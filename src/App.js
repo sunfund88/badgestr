9b99c10cc5e08f35f994c0d75ef4a25e5a1aa745
meangsun <sunfund88@gmail.com>
2023-11-09 19:25:05 +0700

    navbar and log-in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
 import Badge from "./components/Badge/Badge";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
+import Navbar from "./components/Navbar/Navbar";
 import { SimplePool, getEventHash, getSignature, nip19 } from 'nostr-tools';
 
 
@@ -18,25 +19,26 @@ window.relays = [
 ].map(r => [r, { read: true, write: true }])
 
 function App() {
+  async function printPub() {
+    console.log(window.nostr)
+    window.nostr._isEnabled = false
+  }
+
   return (
     // <Badge />
     // <Profile />
     <div className="container">
-      <nav>
-        <div className="logo">
-          <a href="/"><h1>Badge<span className="purple">Str</span></h1></a>
-        </div>
-        <div className="nav-btn">
-          <a href="#" className="login">Log in</a>
-        </div>
-      </nav>
+      <Navbar />
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/p/:id' element={<Profile />} />
-          <Route path='/b/:id' element={<Profile />} />
+          <Route path='/b/:id' element={<Badge />} />
         </Routes>
       </Router>
+      {/* 
+      <button className="button" type="button" onClick={() => { printPub() }}>printPub()</button> */}
+
       <footer>
         Made with 💜 by ... <a href="https://next.nostrudel.ninja/#/u/npub12r5wuvggeh77ft00aycf8nfchkrf9avly5xnaepffm6xmsgz7dcqrvm6se" target="_blank">Suntoshi⚡️</a>
       </footer>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,83 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './Navbar.css'
+
+function Navbar() {
+    const [login, setLogin] = useState((localStorage.getItem('login') === 'true'));
+    const [userLogin, setUserLogin] = useState(JSON.parse(localStorage.getItem('user')));
+
+    async function handleLogin() {
+        const tlogin = login
+        setLogin(!login)
+
+        if (!tlogin) {
+            localStorage.setItem('login', 'true');
+            const u = await getProfile()
+            setUserLogin(u)
+            localStorage.setItem('user', JSON.stringify(u));
+
+        }
+        else {
+            localStorage.setItem('login', 'false');
+            setUserLogin({})
+            localStorage.setItem('user', JSON.stringify({}));
+        }
+
+        // console.log(login)
+    }
+
+    async function getProfile() {
+        try {
+            const pubkey = await window.nostr.getPublicKey()
+            console.log("try...",)
+            const events = await window.pool.list(getReadRelays(), [{
+                kinds: [0],
+                authors: [pubkey]
+            }])
+            // console.log(events)
+
+
+            if (events.length > 0) {
+                let p = {}
+                events.sort((a, b) => b.created_at - a.created_at)
+
+                p = JSON.parse(events[0].content)
+                // console.log(p)
+                return p
+            }
+
+            // console.log(events.length)
+        }
+        catch (error) {
+            // check what is logging here
+            console.log("error in fetchLogin", error)
+            return error.response;
+        }
+
+    }
+
+    function getReadRelays() {
+        return window.relays.filter(r => r[1].read).map(r => r[0])
+    }
+
+    return (
+        <nav>
+            <div className="logo">
+                <a href="/"><h1>Badge<span className="purple">Str</span></h1></a>
+            </div>
+
+            {(login === true)
+                ?
+                <div className="nav-user">
+                    {userLogin?.display_name} <img src={userLogin?.picture} width="30" height="30" alt={userLogin?.name} onError={event => {
+                        event.target.src = "https://developers.google.com/static/maps/documentation/streetview/images/error-image-generic.png"
+                        event.onerror = null
+                    }} />
+                    <button className="nav-btn" type="button" onClick={() => { handleLogin() }}>Log out</button>
+                </div>
+                : <button className="nav-btn" type="button" onClick={() => { handleLogin() }}>Log in</button>}
+        </nav>
+    )
+}
+
+
+export default Navbar
