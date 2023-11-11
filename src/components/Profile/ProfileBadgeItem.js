fa2bfe5ed92f78681d316097f28e1154d0c1f389
meangsun <sunfund88@gmail.com>
2023-11-11 11:22:08 +0700

    profile-badge ui

--- src/components/Profile/ProfileBadgeItem.js
+++ src/components/Profile/ProfileBadgeItem.js
@@ -4,6 +4,7 @@ import { nip19 } from "nostr-tools";
 
 const ProfileBadgeItem = ({ id, badgeItem, type }) => {
     const navigate = useNavigate();
+
     const handleClick = () => {
         // console.log("Image clicked!");
 
@@ -35,17 +36,18 @@ const ProfileBadgeItem = ({ id, badgeItem, type }) => {
 
     // console.log()
     return (
-        <>
-            <li key={id}>
-                <img className="image" src={badgeItem.thumb} width="120" height="120" title={badgeItem.name} alt={badgeItem.name}
-                    onError={event => {
-                        event.target.src = "https://developers.google.com/static/maps/documentation/streetview/images/error-image-generic.png"
-                        event.onerror = null
-                    }} onClick={handleClick} />{badgeItem.name}
+        <div className="badges-item" key={id} onClick={handleClick} >
+            <img className="image" src={badgeItem.thumb} width="120" height="120" title={badgeItem.name} alt={badgeItem.name}
+                onError={event => {
+                    event.target.src = "https://developers.google.com/static/maps/documentation/streetview/images/error-image-generic.png"
+                    event.onerror = null
+                }} />
+            <div className="badges-item-name">
+                {badgeItem.name}
+            </div>
 
-                <div className="text"></div>
-            </li>
-        </>
+            {/* <div className="text"></div> */}
+        </div>
     );
 }
 
--- src/components/Profile/ProfileBadges.js
+++ src/components/Profile/ProfileBadges.js
@@ -1,40 +1,37 @@
 import ProfileBadgeItem from "./ProfileBadgeItem";
 
+
 const ProfileBadges = ({ badgesObj, diffObj }) => {
     return (
         <div className="badges">
             {(badgesObj.length > 0)
-                ? (<h3>Accepted Badges</h3>)
+                ? (
+                    <>
+                        <h3>Accept Badges</h3>
+                        <div className="badges-container">
+                            <div className="badges-list">
+                                {badgesObj.map((b, i) => (
+                                    <ProfileBadgeItem key={i} badgeItem={b} type={true} />
+                                ))}
+                            </div>
+                        </div>
+                    </>
+                )
                 : (<></>)}
-            <div className="badges-container">
-                <div className="badges-list">
-                    <ul>
-                        {badgesObj.map((b, i) => (
-                            <ProfileBadgeItem key={i} badgeItem={b} type={true} />
-                        ))}
-                    </ul>
-                </div>
-            </div>
             {(diffObj.length > 0)
-                ? (<h3>Unaccept Badges</h3>)
+                ? (
+                    <>
+                        <h3>Unaccept Badges</h3>
+                        <div className="badges-container">
+                            <div className="badges-list">
+                                {diffObj.map((b, i) => (
+                                    <ProfileBadgeItem key={i} badgeItem={b} type={false} />
+                                ))}
+                            </div>
+                        </div>
+                    </>
+                )
                 : (<></>)}
-            <div className="badges-container">
-                <div className="badges-list">
-                    <ul>
-                        {diffObj.map((b, i) => (
-                            <li key={i}>
-                                <img className="image" src={b.thumb} width="120" height="120" title={b.name} alt={b.name}
-                                    onError={event => {
-                                        event.target.src = "https://developers.google.com/static/maps/documentation/streetview/images/error-image-generic.png"
-                                        event.onerror = null
-                                    }} />{b.name}
-
-                                <div className="text"></div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
         </div>
     );
 }
