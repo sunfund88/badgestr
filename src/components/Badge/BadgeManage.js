34eaf3d4d130913a0dd34f8d6cb7723a60ee51a9
meangsun <sunfund88@gmail.com>
2023-11-17 13:47:04 +0700

    Create BadgeManage.js

---
+++ src/components/Badge/BadgeManage.js
@@ -0,0 +1,16 @@
+import React, { useRef } from 'react';
+import { useNavigate } from "react-router-dom";
+
+const BadgeManage = () => {
+    const navigate = useNavigate();
+
+
+
+    return (
+        <>
+            <h3>Manage Badge</h3>
+        </>
+    );
+}
+
+export default BadgeManage
